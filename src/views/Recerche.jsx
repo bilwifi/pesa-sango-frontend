84227978f3283e998dfe932a0990c8bdd436a71d
Francois Disubi <disubif@gmail.com>
2020-06-13 00:13:26 +0200

    fist version sgnaler et recherche

---
+++ src/views/Recerche.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form, Container,  Input } from 'semantic-ui-react'
+import Header from "../components/Header"
+
+
+const Recherche = () => {
+    return (
+        <>
+            <Header />
+            <Container>
+                <h1>Rechercher</h1>
+                {/* <Form> */}
+                <Input fluid icon='search' placeholder='Rechercher' />
+                {/* </Form> */}
+            </Container>
+        </>
+    )
+}
+
+
+export default Recherche;
